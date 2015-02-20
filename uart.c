764acff95759b1fbcdbf3e3aef67f7cacade4f92
blinch89 <ulrich.terhorst@gmail.com>
2015-02-20 19:07:25 +0100

    some comments refactored and uart interrupt method cleaned out

--- uart.c
+++ uart.c
@@ -4,74 +4,55 @@
 #include "stm32f4xx_rcc.h"
 void init_USART1(uint32_t baudrate)
 {
-	
-	/* This is a concept that has to do with the libraries provided by ST
-	 * to make development easier the have made up something similar to 
-	 * classes, called TypeDefs, which actually just define the common
-	 * parameters that every peripheral needs to work correctly
-	 * 
-	 * They make our life easier because we don't have to mess around with 
-	 * the low level stuff of setting bits in the correct registers
-	 */
-	GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
-	USART_InitTypeDef USART_InitStruct; // this is for the USART1 initilization
-	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
-	
-	/* enable APB2 peripheral clock for USART1 
-	 * note that only USART1 and USART6 are connected to APB2
-	 * the other USARTs are connected to APB1
-	 */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	
-	/* enable the peripheral clock for the pins used by 
-	 * USART1, PB6 for TX and PB7 for RX
-	 */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-	
-	/* This sequence sets up the TX and RX pins 
-	 * so they work correctly with the USART1 peripheral
-	 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // Pins 6 (TX) and 7 (RX) are used
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF; 			// the pins are configured as alternate function so the USART peripheral has access to them
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// this defines the IO speed and has nothing to do with the baudrate!
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;			// this defines the output type as push pull mode (as opposed to open drain)
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// this activates the pullup resistors on the IO pins
-	GPIO_Init(GPIOB, &GPIO_InitStruct);					// now all the values are passed to the GPIO_Init() function which sets the GPIO registers
-	
-	/* The RX and TX pins are now connected to their AF
-	 * so that the USART1 can take over control of the 
-	 * pins
-	 */
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1); //
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
-	
-	/* Now the USART_InitStruct is used to define the 
-	 * properties of USART1 
-	 */
-	USART_InitStruct.USART_BaudRate = baudrate;				// the baudrate is set to the value we passed into this init function
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;// we want the data frame size to be 8 bits (standard)
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;		// we want 1 stop bit (standard)
-	USART_InitStruct.USART_Parity = USART_Parity_No;		// we don't want a parity bit (standard)
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // we don't want flow control (standard)
-	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // we want to enable the transmitter and the receiver
-	USART_Init(USART1, &USART_InitStruct);					// again all the properties are passed to the USART_Init function which takes care of all the bit setting
-	
-	
-	/* Here the USART1 receive interrupt is enabled
-	 * and the interrupt controller is configured 
-	 * to jump to the USART1_IRQHandler() function
-	 * if the USART1 receive interrupt occurs
-	 */
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // enable the USART1 receive interrupt 
-	
-	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;		 // we want to configure the USART1 interrupts
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;// this sets the priority group of the USART1 interrupts
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;		 // this sets the subpriority inside the group
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			 // the USART1 interrupts are globally enabled
-	NVIC_Init(&NVIC_InitStructure);							 // the properties are passed to the NVIC_Init function which takes care of the low level stuff	
+    
+    GPIO_InitTypeDef GPIO_InitStruct;    //GPIO pins used as TX and RX
+    USART_InitTypeDef USART_InitStruct;  //USART1 initilization
+    NVIC_InitTypeDef NVIC_InitStructure; //configure NVIC (nested vector interrupt controller)
+    
+    // enable APB2 peripheral clock for USART1 
+    // note that only USART1 and USART6 are connected to APB2
+    // the other USARTs are connected to APB1
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    
+    // enable the peripheral clock for the pins used by 
+    // USART1, PB6 for TX and PB7 for RX
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+    
+    // This sequence sets up the TX and RX pins to be
+    // used by USART1 peripheral
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // Pins 6 (TX) and 7 (RX)
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;           // alternate function so the USART peripheral has access to them
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;      // this defines the IO speed and has nothing to do with the baudrate!
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;         // output type = push pull mode (as opposed to open drain)
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;           // activate the pullup resistors
+    GPIO_Init(GPIOB, &GPIO_InitStruct);
+    
+    // The RX and TX pins are now connected to their AF
+    // so USART1 can take over control of the pins
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+    
+    // USART_InitStruct is used to define the properties of USART1 
+    USART_InitStruct.USART_BaudRate = baudrate;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b; // data frame size = 8 bits (standard)
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;      // 1 stop bit (standard)
+    USART_InitStruct.USART_Parity = USART_Parity_No;         // no parity bit (standard)
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // no flow control (standard)
+    USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // enable transmitter & receiver
+    USART_Init(USART1, &USART_InitStruct);
+    
+    
+    // USART1 receive interrupt enable
+    // interrupt controller config     
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // enable USART1 receive interrupt 
+    
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;        // following param. are meant for USART1 interr.
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;// set priority group of the USART1 interrupts
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;       // set subpriority inside group
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;          // USART1 interrupts are globally enabled
+    NVIC_Init(&NVIC_InitStructure);   
 
-	// finally this enables the complete USART1 peripheral
-	USART_Cmd(USART1, ENABLE);
+    USART_Cmd(USART1, ENABLE); // finally this enables the complete USART1 peripheral
 }
 
 
@@ -80,14 +61,12 @@ void init_USART1(uint32_t baudrate)
 
 void USART_puts(USART_TypeDef* USARTx, volatile char *s)
 {
-
-	while(*s)
-	{
-		// wait until data register is empty
-		while( !(USARTx->SR & 0x00000040) ); 
-		USART_SendData(USARTx, *s);
-		*s++;
-	}
+    while(*s)
+    {
+        // wait until data register is empty
+        while( !(USARTx->SR & 0x00000040) ); 
+        USART_SendData(USARTx, *s++);
+    }
 }
 
 
@@ -95,46 +74,19 @@ void USART_puts(USART_TypeDef* USARTx, volatile char *s)
 
 void USART_putc(USART_TypeDef* USARTx, char c)
 {
-	// wait until data register is empty
-	while( !(USARTx->SR & 0x00000040) ); 
-	USART_SendData(USARTx, c);
-	//USART_puts(USART1,"\r\n");
+    // wait until data register is empty
+    while( !(USARTx->SR & 0x00000040) ); 
+    USART_SendData(USARTx, c);
 }
 
 
 
-
+// This method is called, if an USART1 interrupt occures.
 void USART1_IRQHandler()
 {
-	// check if the USART1 receive interrupt flag was set
-	if( USART_GetITStatus(USART1, USART_IT_RXNE) )
-	{
-		switch(USART1->DR)
-		{
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-		case 'A':
-		case 'B':
-		case 'C':
-		case 'D':
-			//control = USART1->DR;
-			//changeFlag = 1;
-			break;
-		case '4':	//stereo off - PinB4;
-			GPIOB->BSRRH = 0x10;
-			break;
-		case '5':	//stereo on - PinB4
-			GPIOB->BSRRL = 0x10;
-			break;
-		default:
-			//changeFlag = 1;
-			break;
-		}
-	}
+    // check if the USART1 receive interrupt flag was set
+    if( USART_GetITStatus(USART1, USART_IT_RXNE) )
+    {
+        // USART1->DR   this line reads the received data
+    }
 }
